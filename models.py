d264c1beab9724bf8af4061e120b82bd6f74278f
DChanna <72782213+DChanna@users.noreply.github.com>
2025-07-09 09:58:31 -0700

    Update models.py via Tesslate Studio

---
+++ models.py
@@ -0,0 +1,14 @@
+
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = "items"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
